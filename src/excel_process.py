98f5214b602eff05f386455e66edf9f8874cca10
WangTN <wangtheneighbor@gmail.com>
2024-10-15 21:01:57 +0800

    Revert "test fix pyinstaller build bug"
    This reverts commit adca8a40ef09b3af6c87adf0fb6a24f3fc49a328.

--- src/excel_process.py
+++ src/excel_process.py
@@ -4,7 +4,6 @@ import tkinter as tk
 from tkinter import simpledialog, messagebox, Scrollbar
 import openpyxl
 import datetime
-import pywintypes
 from win32com.client import DispatchEx
 import random
 import re
